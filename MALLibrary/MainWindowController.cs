aece7d20c4235e9ce166297ccd4c95ac804d9be1
桐間紗路 <kirimasharo@tongjianshalus-MacBook-Pro.local>
2017-02-22 14:31:28 -0800

    Main interface changes.

--- MALLibrary/MainWindowController.cs
+++ MALLibrary/MainWindowController.cs
@@ -2,6 +2,7 @@
 
 using Foundation;
 using AppKit;
+using CoreGraphics;
 
 namespace MALLibrary
 {
@@ -24,9 +25,119 @@ namespace MALLibrary
 		public override void AwakeFromNib()
 		{
 			base.AwakeFromNib();
-			base.Window.Delegate = new MainWindowDelegate();
-		}
+			// Use Unified Toolbar Title Bar
+			base.Window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+			base.Window.SetFrame(base.Window.Frame,true);
+			//Fix Source List Icons to use as templates
+			string[] images = new string[4] { "library", "search", "animeinfo", "seasons"};
+			for (int i = 0; i < 4; i++){
+				NSImage.ImageNamed(images[i]).Template = true;
+			}
+			// Set Main Views to be resizable
+			searchview.AutoresizingMask = NSViewResizingMask.HeightSizable | NSViewResizingMask.WidthSizable;
+			listview.AutoresizingMask = NSViewResizingMask.HeightSizable | NSViewResizingMask.WidthSizable;
+			//Set up list view
+			sourcelist.Initialize();
 
+			var library = new SourceListItem("LIBRARY");
+			library.AddItem("Anime List", NSImage.ImageNamed("library"));
+			sourcelist.AddItem(library);
+			var discover = new SourceListItem("DISCOVER");
+			discover.AddItem("Search", NSImage.ImageNamed("search"));
+			discover.AddItem("Title Info", NSImage.ImageNamed("animeinfo"));
+			discover.AddItem("Seasons", NSImage.ImageNamed("seasons"));
+			sourcelist.AddItem(discover);
+
+			// Display side list
+			sourcelist.ReloadData();
+			sourcelist.ExpandItem(null, true);
+
+			// Retrieve last used main view
+			if (NSUserDefaults.StandardUserDefaults.ValueForKey(new NSString("selectedmainview")) != null)
+			{
+				NSNumber selected = (NSNumber)NSUserDefaults.StandardUserDefaults.ValueForKey(new NSString("selectedmainview"));
+				sourcelist.SelectRow(selected.NIntValue, false);
+			}
+			else
+			{
+				sourcelist.SelectRow(1, true);
+			}
+			this.loadmainview();
+
+		}
+		partial void selectmainview(Foundation.NSObject sender)
+		{
+			this.loadmainview();
+		}
+		private void loadmainview()
+		{
+			
+			CGRect mainviewframe = mainview.Frame;
+			mainview.AddSubview(new NSView());
+			nint selectedrow = sourcelist.SelectedRow;
+			CGPoint origin = new CGPoint();
+			origin.X = 0;
+			origin.Y = 0;
+			var selecteditem = (SourceListItem)sourcelist.ItemAtRow(sourcelist.SelectedRow);
+			switch (selecteditem.Title)
+			{
+				case "Anime List":
+					mainview.ReplaceSubviewWith(mainview.Subviews[0], listview);
+					listview.Frame = mainviewframe;
+					listview.SetFrameOrigin(origin);
+					//toolbar.InsertItem("filter", 2);
+				break;
+				case "Search":
+					mainview.ReplaceSubviewWith(mainview.Subviews[0], searchview);
+					searchview.Frame = mainviewframe;
+					searchview.SetFrameOrigin(origin);
+					listview.SetFrameOrigin(origin);
+					break;
+				case "Title Info":
+					mainview.ReplaceSubviewWith(mainview.Subviews[0], searchview);
+					searchview.Frame = mainviewframe;
+					searchview.SetFrameOrigin(origin);
+					break;
+				case "Seasons":
+					mainview.ReplaceSubviewWith(mainview.Subviews[0], searchview);
+					searchview.Frame = mainviewframe;
+					searchview.SetFrameOrigin(origin);
+					break;
+			}
+			this.createtoolbar();
+			// Save Current Main View
+			NSUserDefaults.StandardUserDefaults.SetValueForKey(new NSNumber(sourcelist.SelectedRow), new NSString("selectedmainview"));
+		}
+		public void createtoolbar()
+		{
+
+			NSToolbarItem[] items = toolbar.Items;
+			if (items != null)
+			{
+				for (nint i = 0; i < items.Length; i++)
+				{
+					toolbar.RemoveItem(0);
+				}
+			}
+			// Create toolbar
+			var selecteditem = (SourceListItem)sourcelist.ItemAtRow(sourcelist.SelectedRow);
+			switch (selecteditem.Title)
+			{
+				case "Anime List":
+					toolbar.InsertItem("refresh", 0);
+					toolbar.InsertItem("NSToolbarFlexibleSpaceItem", 1);
+					toolbar.InsertItem("filter", 2);
+					break;
+				case "Search":
+					toolbar.InsertItem("NSToolbarFlexibleSpaceItem", 0);
+					toolbar.InsertItem("search", 1);
+					break;
+				case "Title Info":
+					break;
+				case "Seasons":
+					break;
+			}
+		}
 		public new MainWindow Window
 		{
 			get { return (MainWindow)base.Window; }
--- MALLibrary/MainWindowController.designer.cs
+++ MALLibrary/MainWindowController.designer.cs
@@ -1,6 +1,6 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
+// This file has been generated automatically by Xamarin Studio to store outlets and
 // actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
@@ -16,24 +16,78 @@ namespace MALLibrary
 		AppKit.NSSearchField Filter { get; set; }
 
 		[Outlet]
+		AppKit.NSView listview { get; set; }
+
+		[Outlet]
+		AppKit.NSView mainview { get; set; }
+
+		[Outlet]
+		AppKit.NSSearchField searchbox { get; set; }
+
+		[Outlet]
+		AppKit.NSView searchview { get; set; }
+
+		[Outlet]
+		MALLibrary.SourceListView sourcelist { get; set; }
+
+		[Outlet]
 		AppKit.NSScrollView tableview { get; set; }
+
+		[Outlet]
+		AppKit.NSToolbar toolbar { get; set; }
 
 		[Action ("performfilter:")]
 		partial void performfilter (Foundation.NSObject sender);
 
 		[Action ("performrefresh:")]
 		partial void performrefresh (Foundation.NSObject sender);
+
+		[Action ("performsearch:")]
+		partial void performsearch (Foundation.NSObject sender);
+
+		[Action ("selectmainview:")]
+		partial void selectmainview (Foundation.NSObject sender);
 		
 		void ReleaseDesignerOutlets ()
 		{
-			if (tableview != null) {
-				tableview.Dispose ();
-				tableview = null;
+			if (toolbar != null) {
+				toolbar.Dispose ();
+				toolbar = null;
 			}
 
 			if (Filter != null) {
 				Filter.Dispose ();
 				Filter = null;
+			}
+
+			if (listview != null) {
+				listview.Dispose ();
+				listview = null;
+			}
+
+			if (mainview != null) {
+				mainview.Dispose ();
+				mainview = null;
+			}
+
+			if (searchbox != null) {
+				searchbox.Dispose ();
+				searchbox = null;
+			}
+
+			if (searchview != null) {
+				searchview.Dispose ();
+				searchview = null;
+			}
+
+			if (sourcelist != null) {
+				sourcelist.Dispose ();
+				sourcelist = null;
+			}
+
+			if (tableview != null) {
+				tableview.Dispose ();
+				tableview = null;
 			}
 		}
 	}

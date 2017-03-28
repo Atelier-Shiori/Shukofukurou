150c63180149cc2b5d8c8669175fbafe21396353
天々座理世 <chikorita157@chikorita157.com>
2017-03-27 21:19:50 -0400

    Add donation dialog

--- MAL Library/DonationWindowController.m
+++ MAL Library/DonationWindowController.m
@@ -8,6 +8,7 @@
 
 #import "DonationWindowController.h"
 #import "Utility.h"
+#import <AFNetworking/AFNetworking.h>
 
 @interface DonationWindowController ()
 
@@ -28,25 +29,30 @@
 -(IBAction)validate:(id)sender{
     if ([[name stringValue] length] > 0 && [[key stringValue] length]>0){
         // Check donation key
-        int success = [Utility checkDonationKey:[key stringValue] name:[name stringValue]];
-        if (success == 1){
-            [Utility showsheetmessage:@"Registered" explaination:@"Thank you for donating. The donation reminder will no longer appear and access to weekly builds is now unlocked." window:nil];
-            // Add to the preferences
-            [[NSUserDefaults standardUserDefaults] setObject:[name stringValue] forKey:@"donor"];
-            [[NSUserDefaults standardUserDefaults] setObject:[key stringValue] forKey:@"donatekey"];
-            [[NSUserDefaults standardUserDefaults] setObject:[NSNumber numberWithBool:YES] forKey:@"donated"];
-            //Close Window
-            [self.window orderOut:self];
-        }
-        else if (success == 2){
+        AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+        manager.requestSerializer = [AFJSONRequestSerializer serializer];
+        manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+        [manager POST:@"https://updates.ateliershiori.moe/keycheck/check.php" parameters:@{@"name":name.stringValue, @"key":key.stringValue} progress:nil success:^(NSURLSessionTask *task, id responseObject) {
+            NSDictionary * d = [NSJSONSerialization JSONObjectWithData:responseObject options:0 error:nil];
+            int valid = [(NSNumber *)d[@"valid"] intValue];
+            if (valid == 1) {
+                // Valid Key
+                [Utility showsheetmessage:@"Registered" explaination:@"Thank you for donating. The donation reminder will no longer appear and access to weekly builds is now unlocked." window:nil];
+                // Add to the preferences
+                [[NSUserDefaults standardUserDefaults] setObject:[name stringValue] forKey:@"donor"];
+                [[NSUserDefaults standardUserDefaults] setObject:[key stringValue] forKey:@"donatekey"];
+                [[NSUserDefaults standardUserDefaults] setObject:[NSNumber numberWithBool:YES] forKey:@"donated"];
+                //Close Window
+                [self.window orderOut:self];
+            }
+            else if (valid == 0){
+                [Utility showsheetmessage:@"Invalid Key" explaination:@"Please make sure you copied the name and key exactly from the email." window:[self window]];
+            }
+
+        } failure:^(NSURLSessionTask *operation, NSError *error) {
+            NSLog(@"%@",error);
             [Utility showsheetmessage:@"No Internet" explaination:@"Make sure you are connected to the internet and try again." window:[self window]];
-        }
-        else{
-            [Utility showsheetmessage:@"Invalid Key" explaination:@"Please make sure you copied the name and key exactly from the email." window:[self window]];
-        }
-    }
-    else{
-            [Utility showsheetmessage:@"Missing Information" explaination:@"Please type in the name and key exactly from the email and try again." window:[self window]];
+                   }];
     }
 }
 

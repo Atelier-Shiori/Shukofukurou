c3ca3eb5fa6414bb74b99f80611ec96d96899b84
桐間紗路 <chikorita157@chikorita157.com>
2017-04-09 19:04:23 -0700

    2.0b2 - Add UI tests, will be worked on soon.

--- MAL LibraryUITests/MAL_LibraryUITests.m
+++ MAL LibraryUITests/MAL_LibraryUITests.m
@@ -34,7 +34,10 @@
 
 - (void)testExample {
     // Use recording to get started writing UI tests.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    
+    
+    
 }
 
 @end

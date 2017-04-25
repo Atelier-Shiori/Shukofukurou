a77e8520de098cfd1b2a68f6996274667b90860b
天々座理世 <chikorita157@chikorita157.com>
2017-04-25 19:11:05 -0400

    2.1 - Use localized date format

---
+++ MAL Library/StringDatetoDate.h
@@ -0,0 +1,13 @@
+//
+//  StringDatetoDate.h
+//  MAL Library
+//
+//  Created by 天々座理世 on 2017/04/25.
+//  Copyright © 2017年 Atelier Shiori. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StringDatetoDate : NSValueTransformer
+
+@end
---
+++ MAL Library/StringDatetoDate.m
@@ -0,0 +1,13 @@
+//
+//  StringDatetoDate.m
+//  MAL Library
+//
+//  Created by 天々座理世 on 2017/04/25.
+//  Copyright © 2017年 Atelier Shiori. All rights reserved.
+//
+
+#import "StringDatetoDate.h"
+
+@implementation StringDatetoDate
+
+@end

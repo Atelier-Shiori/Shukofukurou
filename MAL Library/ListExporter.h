07da09c2857c8b5f7e4fbae22682b69d77762d2f
桐間紗路 <chikorita157@chikorita157.com>
2017-05-09 16:31:05 -0700

    2.1 - Rename list exporter class

--- MAL Library/ListExporter.h
+++ MAL Library/ListExporter.h
@@ -1,15 +1,15 @@
 //
-//  ListImporterExporter.h
+//  ListExporter.h
 //  MAL Library
 //
 //  Created by 桐間紗路 on 2017/05/09.
-//  Copyright © 2017 Atelier Shiori. All rights reserved.
+//  Copyright © 2017 Atelier Shiori. All rights reserved. Licensed under 3-clause BSD License
 //
 
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 
-@interface ListImporterExporter : NSObject
+@interface ListExporter : NSObject
 - (IBAction)exportAnimeList:(id)sender;
 - (IBAction)exportMangaList:(id)sender;
 @end
